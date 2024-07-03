7f12fc2e0d5f063f6146fd9a557d942cfb811a30
Atharv Sanjay Jain <atharv.23bcs10113@ms.sst.scaler.com>
2024-07-03 10:20:35 +0530

    post request implemented

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json());
+
 let courses =[
-    {id:1, name:'course1'},
-    {id:2, name:'course2'},
-    {id:3, name:'course3'}
+    {id:1, name:'java'},
+    {id:2, name:'javascript'},
+    {id:3, name:'python'}
 ];
 
 app.get('/courses', (req, res) => {
@@ -14,4 +16,13 @@ app.get('/courses', (req, res) => {
 const port = 3000;
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
+});
+
+app.post('/courses', (req, res) => {
+    const course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.json(course);
 });
